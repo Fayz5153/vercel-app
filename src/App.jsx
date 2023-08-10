e6c1da0b26f657bc95a1815a3dcd513804b3c7a8
xoji2007 <xojiakbar0241@gmail.com>
2023-08-10 10:55:28 +0500

    category

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContextMode, { MyContext } from './context/context';
 import Header from './pages/header';
 import Main from './pages/main';
-import { BoxLoading } from 'react-loadingg';
+import {Routes, Route, Link} from "react-router-dom"
 
 const App = () => {
   return ( 
@@ -11,13 +11,20 @@ const App = () => {
         {(value)=>{
           return(
             <div>
-              <BoxLoading />
               <button onClick={value.handleTheme}>
                 Theme
-                </button>
+              </button>
               <h1>{value.theme ? "theme dark" : "theme light"}</h1>
-              <Header/>
-              <Main value={value}/>
+              <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/category/book"}>Book</Link>
+                <Link to={"/category/tv"}>Tv</Link>
+              </div>
+              <Routes>
+                <Route index element={<Header/>}/>
+                <Route path='category/:name' element={<Main value={value}/>}/>
+              </Routes>
+              
             </div>
           )
         }}
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { WaveLoading } from 'react-loadingg';
 
-const Main = (props) => {
-    console.log(props)
+const Main = () => {
+    let params = useParams().name
+    // console.log(params)
+    let BASE_URL = "https://64d47322b592423e46942188.mockapi.io"
+    const [data, setData]= useState([])
+    const [load, setLoad]= useState(false)
+
+    useEffect(()=>{
+        setLoad(true)
+        axios.get(BASE_URL+"/"+params).then((res)=>{
+            // console.log(res)
+            setData(res.data)
+            if (res.status === 200) {
+                setTimeout(() => {
+                    setLoad(false)
+                }, 1500);
+            }
+        })
+    },[params])
     return ( 
         <React.Fragment>
-            <h1 className={props.value.theme ? "dark" : "light"}>Main</h1>
+            {load === true ? <div className='load'><WaveLoading/></div> : ""}
+            <div className='grid'>
+                {data.map((item)=>{
+                    return(
+                        <div className='card'>
+                            <img src={item.img} alt="" width={"90%"}/>
+                            <h2>{item.title}</h2>
+                            <h3>{item.price}</h3>
+                        </div>
+                    )
+                })}
+            </div>
         </React.Fragment>
      );
 }
